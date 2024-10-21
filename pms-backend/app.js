42155f403a470f3fb62725f49661fd23a290d784
Mursaleen Nisar <mursaleennisar980@gmail.com>
2024-10-21 21:44:58 +0530

    added cors methods

--- pms-backend/app.js
+++ pms-backend/app.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: `${process.env.FRONTEND_BASE_URL}`,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 // Handle perfight requests
