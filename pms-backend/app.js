f5f096cf67b288391f8e6d7d11f29ae89ab27db8
Mursaleen Nisar <mursaleennisar980@gmail.com>
2024-10-22 12:49:07 +0530

    allowed only frontend of this app to access the backend

--- pms-backend/app.js
+++ pms-backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: '*',
+    origin: `${process.env.FRONTEND_BASE_URL}`,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
